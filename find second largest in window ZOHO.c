3fec4756c68ad284f5c7d0d8705326f44e01e595
SAMYUKTHA <38600655+samyukthagopalsamy@users.noreply.github.com>
2019-11-17 17:03:34 +0530

    Update find second largest in window ZOHO.c

--- find second largest in window ZOHO.c
+++ find second largest in window ZOHO.c
@@ -6,20 +6,27 @@ int main()
 {
    int arr[] = {10,19,30,23,14,27,28,33,32,21,45,41,6,3,90};
    int fl,sl,tl, ind,window=6, size, window_begin, window_end;
-   size = sizeof(arr) / sizeof(arr[0]);
+   /*
+   fl- first largest
+   sl- second largest
+   tl- third largest
+   */
+   size = sizeof(arr) / sizeof(arr[0]); // calculating size of the array
    fl = sl = tl = INT_MIN;
-   for(ind = 0; ind < window; ind++)
+   for(ind = 0; ind < window; ind++)// updating first, second and third largest values for first window (i.e 0 to window-1)
    {
-       if(arr[ind] > fl)
+       if(arr[ind] > fl) 
        {
            tl = sl;
            sl = fl;
            fl = arr[ind];
-       }else if(arr[ind] > sl)
+       }
+      else if(arr[ind] > sl)
        {
            tl = sl;
            sl = arr[ind];
-       } else if(arr[ind] > tl)
+       } 
+      else if(arr[ind] > tl)
             tl = arr[ind];
    }
 printf("%d ", sl);
@@ -27,29 +34,35 @@ window_begin = 0;
 window_end = window -1;
 do
 {
+   /* For each new window only the element at beginning of last window
+   and element at end of new window need to be compared with fl and sl*/
     if(arr[window_begin] == sl)
     {
         sl = tl;
-    } else if(arr[window_begin] == fl)
+    } 
+   else if(arr[window_begin] == fl)
     {
         fl = sl;
         sl = tl;
         tl = INT_MIN;
     }
     window_end++;
-       if(arr[window_end] > fl)
+       if(arr[window_end] > fl) //the new element included in the window is compared with current fl, sl and tl 
        {
            tl = sl;
            sl = fl;
            fl = arr[window_end];
-       }else if(arr[window_end] > sl)
+       }
+      else if(arr[window_end] > sl)
        {
            tl = sl;
            sl = arr[window_end];
-       } else if(arr[window_end] > tl)
+       } 
+      else if(arr[window_end] > tl)
             tl = arr[window_end];
-            printf("%d ", sl);
+    printf("%d ", sl); // the second largest element of current window is printed
     window_begin++;
 
-}while(window_begin < (size-window));
+}
+   while(window_begin < (size-window)); // To prevent overrunning of window size
 }
